68462956b70bb1bcc12e2124f7de75a737c30fda
Usman Mushtaq <112756872+usmanmq00@users.noreply.github.com>
2025-09-09 13:21:42 -0700

    Functional Approach to print each element in list using Stream with Method Reference.

---
+++ src/com/approach/functional/FunctionalProgramming.java
@@ -0,0 +1,18 @@
+package com.approach.functional;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FunctionalProgramming {
+    public static void main(String[] args) {
+        printAllNumbersInList(Arrays.asList(1,2,3,4,5,6,7,8));
+    }
+
+    private static void printAllNumbersInList(List<Integer> list) {
+        list.stream().forEach(FunctionalProgramming::print);
+    }
+
+    private static void print(int number) {
+        System.out.println(number);
+    }
+}
