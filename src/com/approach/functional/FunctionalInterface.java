d077589664427094c6f8729988a2456b21bd4709
Usman Mushtaq <112756872+usmanmq00@users.noreply.github.com>
2025-09-09 13:34:39 -0700

    Functional Interface Implementation

---
+++ src/com/approach/functional/FunctionalInterface.java
@@ -0,0 +1,5 @@
+package com.approach.functional;
+
+public interface FunctionalInterface {
+    void press();
+}
--- src/com/approach/functional/FunctionalProgramming.java
+++ src/com/approach/functional/FunctionalProgramming.java
@@ -5,7 +5,10 @@ import java.util.List;
 
 public class FunctionalProgramming {
     public static void main(String[] args) {
-        printAllNumbersInList(Arrays.asList(1,2,3,4,5,6,7,8));
+//        printAllNumbersInList(Arrays.asList(1,2,3,4,5,6,7,8));
+
+        FunctionalInterface funcInterface = () -> System.out.println("Functional Interface");
+        funcInterface.press();
     }
 
     private static void printAllNumbersInList(List<Integer> list) {
