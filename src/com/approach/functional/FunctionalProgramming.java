07a3e48f9af4934b910df9961b0a29d0d09d7f13
Usman Mushtaq <112756872+usmanmq00@users.noreply.github.com>
2025-09-09 15:12:34 -0700

    Functional way to print even number in list.

--- src/com/approach/functional/FunctionalProgramming.java
+++ src/com/approach/functional/FunctionalProgramming.java
@@ -5,11 +5,22 @@ import java.util.List;
 
 public class FunctionalProgramming {
     public static void main(String[] args) {
-        printAllNumbersInList(Arrays.asList(1,2,3,4,5,6,7,8));
+//        printAllNumbersInList(Arrays.asList(1,2,3,4,5,6,7,8));
+        printEvenNumbersInList(Arrays.asList(1,2,3,4,5,6,7,8));
+    }
+
+    private static void printEvenNumbersInList(List<Integer> list) {
+        list.stream().
+                filter(FunctionalProgramming::isEven).
+                forEach(System.out::println);
     }
 
     private static void printAllNumbersInList(List<Integer> list) {
         list.stream().forEach(System.out::println);   // System.out has a static method println
+    }
+
+    private static boolean isEven(int number) {
+        return number % 2 == 0;
     }
 
 }
