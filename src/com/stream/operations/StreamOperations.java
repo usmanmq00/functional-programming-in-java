5e080d61b91c93f07aacecb30f0974495457ebdf
Usman Mushtaq <112756872+usmanmq00@users.noreply.github.com>
2025-09-10 15:45:15 -0700

    Create stream to get square of even numbers in the list using map.

--- src/com/stream/operations/StreamOperations.java
+++ src/com/stream/operations/StreamOperations.java
@@ -7,6 +7,7 @@ public class StreamOperations {
         List<Integer> numbers = List.of(1,2,3,4,5,6,7,8);
         numbers.stream()
                 .filter(number -> number % 2 == 0)   // Expects a Predicate
+                .map(number -> number*number)    // Expects a Function
                 .forEach(System.out::println);   // Expects a Consumer
     }
 }
